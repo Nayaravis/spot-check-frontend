aafc9264bf40907d2aaf829ab74717ee9dd5fe93
Medy <mannomedy@gmail.com>
2025-09-25 15:43:08 +0300

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+
 function App() {
   return (
-    <>
-      
-    </>
+   
+      <Route path="/" element={<HomePage />} />
+   
   )
 }
 
